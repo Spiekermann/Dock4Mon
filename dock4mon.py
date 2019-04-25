da62dbe397185f27d901c28df2a12e4e22f39fe6
Daniel Spiekermann <dsp@Daniels-MacBook-Air.local>
2019-04-25 14:34:53 +0200

    Added capture function

--- dock4mon.py
+++ dock4mon.py
@@ -7,7 +7,19 @@ worker=set()        # All workers of the investigated swarm
 noworker=set()      # hosts that are not a worker
 mapping={}          # Store the ips of the hosting systems
 
+# capture process
+# implement your own technique
+# in this poc, we use ssh-based iptables manipulation
+def capture(ip):
+    print "Capture for " + ip
 
+
+# remove capture process
+# just remove the things you've done before
+def redo(ip):
+    print "Remove capture for " + ip
+
+# monitoring process
 def monitor():
     time.sleep(1)
     c=shlex.split("docker node ls --format \"{{.ID}}:{{.ManagerStatus}}:{{.Availability}}:{{.Status}}\"")  # type: str
@@ -27,7 +39,8 @@ def monitor():
         else:   # Manager, check for worker capabilities
             if u[3] == "Down":  # Worker was in swarm, delete it from set
                 if u[0] in worker:
-                    print "Please remove worker " + mapping[u[0]] + " from capture."
+                    redo(mapping[u[0]])
+                  #  print "Please remove worker " + mapping[u[0]] + " from capture."
                     worker.remove(u[0])
                     print "Swarm has " + str(len(worker)) + " members"
 
@@ -43,6 +56,7 @@ def inspect(worker):
             if "Addr" in x:
                 if worker not in mapping:
                     print "New member of swarm found."
+                    capture(x.split(":")[1])
                     print "Create capture process on host: " + x.split(":")[1]
                     mapping[worker]=x.split(":")[1]
                     print "Swarm has " + str(len(worker)) + " members"
